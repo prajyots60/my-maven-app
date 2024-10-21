ca6ff11adf3d2857162a82dfb00e8a5d33bc6ec2
Prajyot Shinde <prajyotshinde60@gmail.com>
2024-10-21 18:30:43 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven!");
+        System.out.println("I am Prajyot Shinde testing maven project");
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertTrue("app should have a greeting", true);
+    }
+}
